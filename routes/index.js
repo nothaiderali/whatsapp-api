634f01279ffbd63d800214e0af86b1be2ed54452
Haider Ali <73281382+nothaiderali@users.noreply.github.com>
2023-02-15 22:36:59 +0500

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,49 @@
 'use strict';
 const router = require('express').Router();
 
-router.get('/meta_wa_callbackurl', (req, res) => {
+const WhatsappCloudAPI = require('whatsappcloudapi_wrapper');
+const Whatsapp = new WhatsappCloudAPI({
+    accessToken: process.env.Meta_WA_accessToken,
+    senderPhoneNumberId: process.env.Meta_WA_SenderPhoneNumberId,
+    WABA_ID: process.env.Meta_WA_wabaId, 
+    graphAPIVersion: 'v14.0'
+});
+
+const EcommerceStore = require('./../utils/ecommerce_store.js');
+let Store = new EcommerceStore();
+const CustomerSession = new Map();
+
+
+router.get('/meta_wa_callbackurl', async (req, res) => {
     try {
+        let data = Whatsapp.parseMessage(req.body);
+
+        if (data?.isMessage) {
+            let incomingMessage = data.message;
+            let recipientPhone = incomingMessage.from.phone; // extract the phone number of sender
+            let recipientName = incomingMessage.from.name;
+            let typeOfMsg = incomingMessage.type; // extract the type of message (some are text, others are images, others are responses to buttons etc...)
+            let message_id = incomingMessage.message_id; // extract the message id
+
+            if (typeOfMsg === 'text_message') {
+                await Whatsapp.sendSimpleButtons({
+                    message: `Hey ${recipientName}, \nYou are speaking to a chatbot.\nWhat do you want to do next?`,
+                    recipientPhone: recipientPhone, 
+                    listOfButtons: [
+                        {
+                            title: 'View some products',
+                            id: 'see_categories',
+                        },
+                        {
+                            title: 'Speak to a human',
+                            id: 'speak_to_human',
+                        },
+                    ],
+                });
+            }
+        }
+        
+
         console.log('GET: Someone is pinging me!');
 
         let mode = req.query['hub.mode'];
