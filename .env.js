847a4f8a48924108bb16ab1ad77016170703591a
Haider Ali <73281382+nothaiderali@users.noreply.github.com>
2023-02-15 23:13:38 +0500

    Update .env.js

--- .env.js
+++ .env.js
@@ -7,7 +7,7 @@ const development = {
     ...process.env,
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: '9000',
-    Meta_WA_accessToken:'EABLtwfrbyC0BAFNJdN1Oq6es1hUhycGWpK1C4iLU4XfUM9hvMZAM3Ul5om62ZBC9OogDhbzKZBgcyZBlLjPApeP7vBlyzeJOXXOZBgB1hZAZCRJ5VCiGCuHTpe0XaCCyAzCefgDGI3sjOrXuliWkoeVcyzllkxpBqe5SDDi28YjZASeE4nEPqZCnzSTgF1qgseY1aRVoRSlYZCkQZDZD',
+    Meta_WA_accessToken:'EABLtwfrbyC0BADuFamFdnQyXkI5NMFnWnanGXd0cWEWMy8u3mx056j3NOqwv7xQ8MVHrbZBiqL5nB1L6HncdlLKgH0D69Hb4nWilQZCje1fyGeKo5Tej48M3XEA3zaZAZAv1HgxZAfOQZCQL5sHeDmtkN8N8HzudeqQYTWUpNSQsC8MDGChvkDnmZBcZCZCCSw2TAjjHhvUVvsAZDZD',
     Meta_WA_SenderPhoneNumberId: '105055832516532',
     Meta_WA_wabaId: '115541034788584',
     Meta_WA_VerifyToken: 'YouCanSetYourOwnToken',
